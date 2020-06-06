3fac0ae044f66b3304d24760b28047fae378d9fd
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-06-06 16:19:52 -0400

    Implement of Head component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useCallback, useRef } from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <p>Hello</p>
+    <div id="wrap">
+      <Router>
+        <header>
+          <Header />
+        </header>
+        <aside></aside>
+        <section></section>
+        <footer></footer>
+      </Router>
     </div>
   );
 }
