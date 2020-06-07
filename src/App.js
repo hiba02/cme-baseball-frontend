6c33356bde9284f18cda7ca9f8ca815bfc9cf1e7
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-06-07 13:16:02 -0400

    Added CreateTeam component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import CreateTeam from "./components/CreateTeam";
 
 function App() {
   const [word, setWord] = useState("");
@@ -24,6 +25,23 @@ function App() {
     position: "RF",
   });
   const nextId = useRef(100);
+
+  // functions
+
+  //CreateTeam.js handleSubmit
+  const addTeamInClient = useCallback(
+    (teamName, userId) => {
+      const team = {
+        id: nextId.current,
+        name: teamName,
+        user_id: userId,
+      };
+      setTeamNames(teamNames.concat(team));
+      nextId.current += 1;
+    },
+    [teamNames]
+  );
+
   return (
     <div id="wrap">
       <Router>
@@ -33,7 +51,21 @@ function App() {
         <aside>
           <Nav userInfo={userInfo} currentTeam={currentTeam} />
         </aside>
-        <section></section>
+        <section>
+          <Switch>
+            <Route path="/createTeam">
+              <CreateTeam
+                // addTeam={addTeam}
+                // currentUserInfo={userInfo}
+                // curretnUserId={userId}
+                // addFavoriteTeam={addFavoriteTeam}
+                // getUserIdTeamId={getUserIdTeamId}
+                // getUserInfoByEmail={getUserInfoByEmail}
+                addTeamInClient={addTeamInClient}
+              />
+            </Route>
+          </Switch>
+        </section>
         <footer></footer>
       </Router>
     </div>
