b452d9951a482170e4d82cfe6dec8f2a1813539b
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-06-07 12:58:17 -0400

    Added menu in Nav component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,24 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [userId, setUserId] = useState(1);
   const [userInfo, setUserInfo] = useState(null);
+  const [playersInfo, setPlayersInfo] = useState(null);
+  const [teamNames, setTeamNames] = useState(null); //[]
+  const [teamId, setTeamId] = useState(1);
   const [currentTeam, setCurrentTeam] = useState("");
-
+  const [pitcher, setPitcher] = useState({
+    name: "A Aron",
+    uniform_number: "35",
+    position: "RF",
+  });
+  const [hitter, setHitter] = useState({
+    name: "A Aron",
+    uniform_number: "35",
+    position: "RF",
+  });
+  const nextId = useRef(100);
   return (
     <div id="wrap">
       <Router>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,59 +1,56 @@
 import React from "react";
 import "./Nav.scss";
 import "./Slideshow";
-// npm install react-router-dom
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NewWindow from "react-new-window";
 import Slideshow from "./Slideshow";
 
 const Nav = ({ userInfo, currentTeam }) => {
-  console.log("Nav conponent userInfo", userInfo);
-  console.log("Nav conponent currentTeam", currentTeam);
-  let userFirstName = "";
-  if (userInfo) {
-    userFirstName = userInfo.first_name;
-  } else {
-    userFirstName = "user";
-  }
+  // let userFirstName = "";
+  // if (userInfo) {
+  //   userFirstName = userInfo.first_name;
+  // } else {
+  //   userFirstName = "user";
+  // }
 
   return (
     <article className="Nav_component">
       <div className="menu">
-        <p className="Nav-title">Hello, {userFirstName}</p>
+        {/* <p className="Nav-title">Hello, {userFirstName}</p>
         {currentTeam ? <p>Team</p> : ""}
-        {currentTeam ? <p className="Nav-team"> {currentTeam}</p> : ""}
+        {currentTeam ? <p className="Nav-team"> {currentTeam}</p> : ""} */}
         <h4 className="col_title">Menu</h4>
 
-        {userInfo ? (
-          <ul>
-            <li>
-              <Link to="/createTeam" className="menu_link">
-                Create team{" "}
-                <i className="fa fa-angle-double-right" aria-hidden="true"></i>
-              </Link>
-            </li>
-            <li>
-              <Link to="/showTeam" className="menu_link">
-                Show Teams{" "}
-                <i className="fa fa-angle-double-right" aria-hidden="true"></i>
-              </Link>
-            </li>
-            <li>
-              <Link to="/createPlayer" className="menu_link">
-                Create Player{" "}
-                <i className="fa fa-angle-double-right" aria-hidden="true"></i>
-              </Link>
-            </li>
-            <li>
-              <Link to="/showPlayer" className="menu_link">
-                Show Players{" "}
-                <i className="fa fa-angle-double-right" aria-hidden="true"></i>
-              </Link>
-            </li>
-          </ul>
-        ) : (
+        {/* {userInfo ? ( */}
+        <ul>
+          <li>
+            <Link to="/createTeam" className="menu_link">
+              Create team{" "}
+              <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/showTeam" className="menu_link">
+              Show Teams{" "}
+              <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/createPlayer" className="menu_link">
+              Create Player{" "}
+              <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/showPlayer" className="menu_link">
+              Show Players{" "}
+              <i className="fa fa-angle-double-right" aria-hidden="true"></i>
+            </Link>
+          </li>
+        </ul>
+        {/* ) : (
           ""
-        )}
+        )} */}
         <ul>
           <li>
             <Link to="/playball" className="menu_link">
